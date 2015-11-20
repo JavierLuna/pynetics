eeea1192f25457ee8df7ada758e4e4ae0c5e789f
blazaid <alberto.da@gmail.com>
2015-11-20 23:11:35 +0100

    Modified example in main.py

--- pynetics/gggp/main.py
+++ pynetics/gggp/main.py
@@ -1,5 +1,4 @@
 from pprint import pprint
-
 from pynetics.gggp import grammar
 
 awesome_grammar = grammar.Grammar(
@@ -8,31 +7,37 @@ awesome_grammar = grammar.Grammar(
         grammar.Production(
             'frase',
             grammar.And(
-                grammar.ZeroOrOne('sujeto'),
+                grammar.Multiplier('sujeto', upper=1),
                 'predicado',
-                grammar.ZeroOrMore(
+                grammar.Multiplier(
                     grammar.And(
                         'conjuncion',
-                        grammar.ZeroOrOne('sujeto'),
+                        grammar.Multiplier('sujeto', upper=1),
                         'predicado'
-                    )
+                    ),
                 )
             ),
         ),
-        grammar.Production('conjuncion', grammar.Or(('y', .4), ('o', .6))),
+        grammar.Production('conjuncion', grammar.Or(('y', 4), ('o', 6))),
         grammar.Production('sujeto', grammar.Or('sujeto_masc', 'sujeto_fem')),
         grammar.Production(
             'sujeto_masc',
             grammar.Or(
                 grammar.And('el', 'nombre_comun_masc'),
-                grammar.And(grammar.ZeroOrOne('el'), 'nombre_propio_masc'),
+                grammar.And(
+                    grammar.Multiplier('el', upper=1),
+                    'nombre_propio_masc'
+                ),
             )
         ),
         grammar.Production(
             'sujeto_fem',
             grammar.Or(
                 grammar.And('la', 'nombre_comun_fem'),
-                grammar.And(grammar.ZeroOrOne('la'), 'nombre_propio_fem'),
+                grammar.And(
+                    grammar.Multiplier('la', upper=1),
+                    'nombre_propio_fem'
+                ),
             )
         ),
         grammar.Production(
@@ -53,7 +58,7 @@ awesome_grammar = grammar.Grammar(
         ),
         grammar.Production(
             'predicado',
-            grammar.And('verbo', grammar.ZeroOrOne('complemento'))
+            grammar.And('verbo', grammar.Multiplier('complemento', upper=1))
         ),
         grammar.Production(
             'verbo',
@@ -65,7 +70,12 @@ awesome_grammar = grammar.Grammar(
                 'va',
                 'come',
                 'dice',
-                'canta'
+                'canta',
+                'caga',
+                'mea',
+                'micciona',
+                'excreta',
+                'evacúa',
             )
         ),
         grammar.Production(
@@ -78,7 +88,8 @@ awesome_grammar = grammar.Grammar(
                 'abriendo puertas',
                 'a las barricadas',
                 'algo',
-                'siempre'
+                'siempre',
+                'a dos manos',
             )
         ),
     ]
