25ac79d5c0103ef7082d2c89d19e41fbae937795
blazaid <alberto.da@gmail.com>
2016-02-18 18:07:17 +0100

    Upgraded minor version due to some architectural changes

--- pynetics/__init__.py
+++ pynetics/__init__.py
@@ -15,7 +15,7 @@ from pynetics.selections import Selection
 from pynetics.stop import StopCondition
 from pynetics.utils import check_is_instance_of, take_chances
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 
 class Population(abc.MutableSequence):
